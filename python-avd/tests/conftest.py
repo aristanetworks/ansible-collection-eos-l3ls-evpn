74f3d24e5277cb2614c0771783c1b2967f4a4a6b
Claus Holbech <holbech@arista.com>
2024-11-18 13:24:07 +0100

    CI: Add examples to pytest

---
+++ python-avd/tests/conftest.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+from pathlib import Path
+from sys import path
+
+path.insert(0, str(Path(__file__).parents[1]))
