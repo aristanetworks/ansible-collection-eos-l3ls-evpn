96ab1699ef431123bb9c43f6589fc4e41a8e1ee8
Claus Holbech <holbech@arista.com>
2024-12-09 12:28:18 +0100

    Try to convince ansible test to work

--- ansible_collections/arista/avd/plugins/plugin_utils/pyavd_wrappers.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/pyavd_wrappers.py
@@ -32,8 +32,10 @@ class RaiseOnUse:
     def __call__(self, *_args: Any, **_kwargs: Any) -> NoReturn:
         raise self.exception
 
-    def __getattr__(self, *_args: Any, **_kwargs: Any) -> NoReturn:
-        raise self.exception
+    def __getattr__(self, name: str) -> NoReturn:
+        if not name.startswith("__"):
+            raise self.exception
+        return self.__getattribute__(name)
 
 
 def wrap_plugin(plugin_type: Literal["filter", "test"], name: str) -> Callable:
