382720e9d87ff936b7bb452f96ec08ae844c48a0
Guillaume Mulocher <gmulocher@arista.com>
2025-01-22 14:28:03 +0100

    Update python-avd/pyavd/_eos_designs/structured_config/mlag/__init__.py

--- python-avd/pyavd/_eos_designs/structured_config/mlag/__init__.py
+++ python-avd/pyavd/_eos_designs/structured_config/mlag/__init__.py
@@ -377,7 +377,6 @@ class AvdStructuredConfigMlag(StructuredConfigGenerator):
 
         router_bgp["peer_groups"] = [strip_empties_from_dict(peer_group)]
 
-        # If self.inputs.mlag_ipv4_vrfs_peer is defined in the input
         if self.shared_utils.use_separate_peer_group_for_mlag_vrfs:
             router_bgp["peer_groups"].append(
                 {
