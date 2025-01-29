b1f5ec388490e7da99474a98f49005ab6378d695
Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>
2025-01-29 17:48:10 +0530

    Update python-avd/pyavd/_eos_designs/structured_config/network_services/dps_interfaces.py
    Co-authored-by: Claus Holbech <holbech@arista.com>

--- python-avd/pyavd/_eos_designs/structured_config/network_services/dps_interfaces.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/dps_interfaces.py
@@ -39,9 +39,8 @@ class DpsInterfacesMixin(UtilsMixin):
         # TODO: do IPv6 when needed - for now no easy way in AVD to detect if this is needed
         # When needed - need a default value if different than IPv4
 
-        flow_tracker = self.shared_utils.new_get_flow_tracker(
+        dps1.flow_tracker = self.shared_utils.new_get_flow_tracker(
             self.inputs.fabric_flow_tracking.dps_interfaces, output_type=EosCliConfigGen.DpsInterfacesItem.FlowTracker
         )
-        dps1._update(flow_tracker=flow_tracker)
 
         self.structured_config.dps_interfaces.append(dps1)
