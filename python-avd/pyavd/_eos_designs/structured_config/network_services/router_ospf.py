743852a67339a2b8cdaf4aef7c508b519ddf9654
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2025-02-05 18:29:24 +0530

    Refactor(eos_designs): structured_config for network_services router_ospf

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_ospf.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_ospf.py
@@ -3,11 +3,12 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING, Protocol
 
+from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 from pyavd._errors import AristaAvdInvalidInputsError
-from pyavd._utils import append_if_not_duplicate, default
+from pyavd._utils import default
 
 if TYPE_CHECKING:
     from . import AvdStructuredConfigNetworkServicesProtocol
@@ -20,18 +21,17 @@ class RouterOspfMixin(Protocol):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def router_ospf(self: AvdStructuredConfigNetworkServicesProtocol) -> dict | None:
+    @structured_config_contributor
+    def router_ospf(self: AvdStructuredConfigNetworkServicesProtocol) -> None:
         """
-        Return structured config for router_ospf.
+        Set structured config for router_ospf.
 
         If we have static_routes in default VRF and not EPVN, and underlay is OSPF
         Then add redistribute static to the underlay OSPF process.
         """
         if not self.shared_utils.network_services_l3:
-            return None
+            return
 
-        ospf_processes = []
         for tenant in self.shared_utils.filtered_tenants:
             for vrf in tenant.vrfs:
                 if not vrf.ospf.enabled:
@@ -40,13 +40,12 @@ class RouterOspfMixin(Protocol):
                 if vrf.ospf.nodes and self.shared_utils.hostname not in vrf.ospf.nodes:
                     continue
 
-                ospf_interfaces = []
+                ospf_interfaces = EosCliConfigGen.RouterOspf.ProcessIdsItem.NoPassiveInterfaces()
                 for l3_interface in vrf.l3_interfaces:
                     if l3_interface.ospf.enabled:
                         for node_index, node in enumerate(l3_interface.nodes):
                             if node != self.shared_utils.hostname:
                                 continue
-
                             ospf_interfaces.append(l3_interface.interfaces[node_index])
 
                 for svi in vrf.svis:
@@ -58,47 +57,31 @@ class RouterOspfMixin(Protocol):
                 if not process_id:
                     msg = f"Missing or invalid 'ospf.process_id' or 'vrf_id' under vrf '{vrf.name}"
                     raise AristaAvdInvalidInputsError(msg)
-
-                process = {
-                    "id": process_id,
-                    "vrf": vrf.name if vrf.name != "default" else None,
-                    "passive_interface_default": True,
-                    "router_id": self.get_vrf_router_id(vrf, vrf.ospf.router_id, tenant.name),
-                    "no_passive_interfaces": ospf_interfaces,
-                    "bfd_enable": vrf.ospf.bfd or None,  # Historic behavior is to only output if True.
-                    "max_lsa": vrf.ospf.max_lsa,
-                }
-
-                process_redistribute = {}
+                process = EosCliConfigGen.RouterOspf.ProcessIdsItem(id=process_id)
+                if vrf.name != "default":
+                    process.vrf = vrf.name
+                process.passive_interface_default = True
+                if vrf_router_id := self.get_vrf_router_id(vrf, vrf.ospf.router_id, tenant.name):
+                    process.router_id = vrf_router_id
+                process.no_passive_interfaces = ospf_interfaces
+                if vrf.ospf.bfd:
+                    process.bfd_enable = vrf.ospf.bfd
+                process.max_lsa = vrf.ospf.max_lsa
 
                 if vrf.ospf.redistribute_bgp.enabled:
-                    process_redistribute["bgp"] = {"enabled": True}
+                    process.redistribute.bgp.enabled = True
                     if route_map := vrf.ospf.redistribute_bgp.route_map:
-                        process_redistribute["bgp"]["route_map"] = route_map
+                        process.redistribute.bgp.route_map = route_map
 
                 if vrf.ospf.redistribute_connected.enabled:
-                    process_redistribute["connected"] = {"enabled": True}
+                    process.redistribute.connected.enabled = True
                     if route_map := vrf.ospf.redistribute_connected.route_map:
-                        process_redistribute["connected"]["route_map"] = route_map
+                        process.redistribute.connected.route_map = route_map
 
-                process["redistribute"] = process_redistribute or None
-
-                # Strip None values from process before adding to list
-                process = {key: value for key, value in process.items() if value is not None}
-
-                append_if_not_duplicate(
-                    list_of_dicts=ospf_processes,
-                    primary_key="id",
-                    new_dict=process,
-                    context="OSPF Processes defined under network services",
-                    context_keys=["id"],
-                )
-
+                self.structured_config.router_ospf.process_ids.append(process)
         # If we have static_routes in default VRF and not EPVN, and underlay is OSPF
         # Then add redistribute static to the underlay OSPF process.
         if self._vrf_default_ipv4_static_routes["redistribute_in_underlay"] and self.shared_utils.underlay_routing_protocol in ["ospf", "ospf-ldp"]:
-            ospf_processes.append({"id": self.inputs.underlay_ospf_process_id, "redistribute": {"static": {"enabled": True}}})
-        if ospf_processes:
-            return {"process_ids": ospf_processes}
-
-        return None
+            process = EosCliConfigGen.RouterOspf.ProcessIdsItem(id=self.inputs.underlay_ospf_process_id)
+            process.redistribute.static.enabled = True
+            self.structured_config.router_ospf.process_ids.append(process)
