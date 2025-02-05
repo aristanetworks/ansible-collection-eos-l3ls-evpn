1f3154f28b12c31709801ec1732c33d5985468d4
Mahesh Kumar <mahesh.kumar-ext@Maheshs-MacBook-Pro.local>
2025-02-05 19:11:15 +0530

    Refactor(eos_designs): structured_config for network_services router_service_insertion

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_service_insertion.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_service_insertion.py
@@ -3,8 +3,10 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING
+
+from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 
 from .utils import UtilsMixin
 
@@ -19,38 +21,31 @@ class RouterServiceInsertionMixin(UtilsMixin):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def router_service_insertion(self: AvdStructuredConfigNetworkServices) -> dict | None:
+    @structured_config_contributor
+    def router_service_insertion(self: AvdStructuredConfigNetworkServices) -> None:
         """
-        Return structured config for router_service_insertion.
+        Set the structured config for router_service_insertion.
 
         Only used for CV Pathfinder edge routers today
         """
         if not self._filtered_internet_exit_policies_and_connections:
-            return None
+            return
 
-        service_connections = []
-
+        service_connections = EosCliConfigGen.RouterServiceInsertion.Connections()
         for _policy, connections in self._filtered_internet_exit_policies_and_connections:
             for connection in connections:
-                service_connection = {
-                    "name": connection["name"],
-                    "monitor_connectivity_host": connection["monitor_name"],
-                }
+                service_connection = EosCliConfigGen.RouterServiceInsertion.ConnectionsItem(
+                    name=connection["name"], monitor_connectivity_host=connection["monitor_name"]
+                )
 
                 if connection["type"] == "tunnel":
-                    service_connection["tunnel_interface"] = {
-                        "primary": f"Tunnel{connection['tunnel_id']}",
-                    }
+                    service_connection.tunnel_interface.primary = f"Tunnel{connection['tunnel_id']}"
+
                 elif connection["type"] == "ethernet":
-                    service_connection["ethernet_interface"] = {
-                        "name": connection["source_interface"],
-                        "next_hop": connection["next_hop"],
-                    }
+                    service_connection.ethernet_interface._update(name=connection["source_interface"], next_hop=connection["next_hop"])
 
                 service_connections.append(service_connection)
 
-        if service_connections:
-            return {"enabled": True, "connections": service_connections}
-
-        return None
+        self.structured_config.router_service_insertion.connections = service_connections
+        if self.structured_config.router_service_insertion.connections:
+            self.structured_config.router_service_insertion.enabled = True
