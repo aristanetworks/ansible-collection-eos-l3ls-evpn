2badaf5681aa3c20fbb9894fd33ba1060ccd4fe6
Laxmikant Chintakindi <laxmikant.c-ext@Laxmikants-MacBook-Pro.local>
2025-02-06 14:24:14 +0530

    Fixing lost configs

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_pim_sparse_mode.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_pim_sparse_mode.py
@@ -33,23 +33,18 @@ class RouterPimSparseModeMixin(Protocol):
             for vrf in tenant.vrfs:
                 if vrf_rps := getattr(vrf, "_pim_rp_addresses", None):
                     ipv4_config = EosCliConfigGen.RouterPimSparseMode.VrfsItem.Ipv4()
-                    rps_item = EosCliConfigGen.RouterPimSparseMode.VrfsItem.Ipv4.RpAddressesItem()
+                    rpaddresses = EosCliConfigGen.RouterPimSparseMode.VrfsItem.Ipv4.RpAddresses()
+
                     for rps in vrf_rps:
+                        rpaddress = EosCliConfigGen.RouterPimSparseMode.VrfsItem.Ipv4.RpAddressesItem()
+                        if "address" in rps:
+                            rpaddress.address = rps["address"]
                         if "groups" in rps:
                             for group in rps["groups"]:
-                                rps_item.groups.append_unique(group)
+                                rpaddress.groups.append(group)
                         if "access_lists" in rps:
                             for access_list in rps["access_lists"]:
-                                rps_item.access_lists.append_unique(access_list)
-                        if "address" in rps:
-                            rps_item.address = rps["address"]
-                        if "priority" in rps:
-                            rps_item.priority = rps["priority"]
-                        if "hashmask" in rps:
-                            rps_item.hashmask = rps["hashmask"]
-                        if "override" in rps:
-                            rps_item.override = rps["override"]
-                        if "_custom_data" in rps:
-                            rps_item._custom_data = rps["_custom_data"]
-                        ipv4_config.rp_addresses.append_unique(rps_item)
+                                rpaddress.access_lists.append(access_list)
+                        rpaddresses.append_unique(rpaddress)
+                    ipv4_config.rp_addresses = rpaddresses
                     self.structured_config.router_pim_sparse_mode.vrfs.append_new(name=vrf.name, ipv4=ipv4_config)
