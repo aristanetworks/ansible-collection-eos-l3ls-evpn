468d927936a695271699a71fbf62a119524527d9
gmuloc <gmulocher@arista.com>
2025-02-07 15:43:42 +0100

    Refactor(eos_designs): Network Services port-channel refactoring to classes

--- python-avd/pyavd/_eos_designs/structured_config/network_services/port_channel_interfaces.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/port_channel_interfaces.py
@@ -4,12 +4,15 @@
 from __future__ import annotations
 
 import re
-from functools import cached_property
 from typing import TYPE_CHECKING, Protocol
 
-from pyavd._utils import append_if_not_duplicate, short_esi_to_route_target
+from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
+from pyavd._utils import short_esi_to_route_target
 
 if TYPE_CHECKING:
+    from pyavd._eos_designs.schema import EosDesigns
+
     from . import AvdStructuredConfigNetworkServicesProtocol
 
 
@@ -20,122 +23,92 @@ class PortChannelInterfacesMixin(Protocol):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def port_channel_interfaces(self: AvdStructuredConfigNetworkServicesProtocol) -> list | None:
+    @structured_config_contributor
+    def port_channel_interfaces(self: AvdStructuredConfigNetworkServicesProtocol) -> None:
         """
-        Return structured config for port_channel_interfaces.
+        Set structured config for port_channel_interfaces.
 
         Only used with L1 network services
         """
         if not self.shared_utils.network_services_l1:
-            return None
+            return
 
-        # Using temp variables to keep the order of interfaces from Jinja
-        port_channel_interfaces = []
-        subif_parent_interfaces = []
+        subif_parent_interfaces: set[EosCliConfigGen.PortChannelInterfacesItem] = set()
+        """Set to collect all the parent interface names of all the subinterfaces defined under l3_interfaces or point_to_point_services in network_services."""
 
         for tenant in self.shared_utils.filtered_tenants:
             if not tenant.point_to_point_services:
                 continue
 
-            for point_to_point_service in tenant.point_to_point_services._natural_sorted():
-                for endpoint in point_to_point_service.endpoints:
-                    if self.shared_utils.hostname not in endpoint.nodes:
-                        continue
+            self._set_point_to_point_interfaces(tenant, subif_parent_interfaces)
 
-                    node_index = endpoint.nodes.index(self.shared_utils.hostname)
-                    interface_name = endpoint.interfaces[node_index]
-                    if (port_channel_mode := endpoint.port_channel.mode) not in ["active", "on"]:
-                        continue
+            for subif_parent_interface in subif_parent_interfaces:
+                if subif_parent_interface not in self.structured_config.port_channel_interfaces:
+                    self.structured_config.port_channel_interfaces.append(subif_parent_interface)
 
-                    channel_group_id = "".join(re.findall(r"\d", interface_name))
-                    interface_name = f"Port-Channel{channel_group_id}"
-                    if point_to_point_service.subinterfaces:
-                        # This is a subinterface so we need to ensure that the parent is created
-                        parent_interface = {
-                            "name": interface_name,
-                            "switchport": {"enabled": False},
-                            "peer_type": "system",
-                            "shutdown": False,
-                        }
-                        if (short_esi := endpoint.port_channel.short_esi) is not None and len(short_esi.split(":")) == 3:
-                            parent_interface.update(
-                                {
-                                    "evpn_ethernet_segment": {
-                                        "identifier": f"{self.inputs.evpn_short_esi_prefix}{short_esi}",
-                                        "route_target": short_esi_to_route_target(short_esi),
-                                    },
-                                },
-                            )
-                            if port_channel_mode == "active":
-                                parent_interface["lacp_id"] = short_esi.replace(":", ".")
+    def _set_point_to_point_interfaces(
+        self: AvdStructuredConfigNetworkServicesProtocol,
+        tenant: EosDesigns._DynamicKeys.DynamicNetworkServicesItem.NetworkServicesItem,
+        subif_parent_interfaces: set[EosCliConfigGen.PortChannelInterfacesItem],
+    ) -> None:
+        """Set the structured_config port_channel_interfaces with the point-to-point interfaces defined under network_services."""
+        for point_to_point_service in tenant.point_to_point_services._natural_sorted():
+            for endpoint in point_to_point_service.endpoints:
+                if self.shared_utils.hostname not in endpoint.nodes:
+                    continue
 
-                        subif_parent_interfaces.append(parent_interface)
+                node_index = endpoint.nodes.index(self.shared_utils.hostname)
+                interface_name = endpoint.interfaces[node_index]
+                if (port_channel_mode := endpoint.port_channel.mode) not in ["active", "on"]:
+                    continue
 
-                        for subif in point_to_point_service.subinterfaces:
-                            subif_name = f"{interface_name}.{subif.number}"
+                channel_group_id = "".join(re.findall(r"\d", interface_name))
+                interface_name = f"Port-Channel{channel_group_id}"
+                if point_to_point_service.subinterfaces:
+                    # This is a subinterface so we need to ensure that the parent is created
+                    parent_interface = EosCliConfigGen.PortChannelInterfacesItem(
+                        name=interface_name,
+                        peer_type="system",
+                        shutdown=False,
+                    )
+                    parent_interface.switchport.enabled = False
 
-                            port_channel_interface = {
-                                "name": subif_name,
-                                "peer_type": "point_to_point_service",
-                                "encapsulation_vlan": {
-                                    "client": {
-                                        "encapsulation": "dot1q",
-                                        "vlan": subif.number,
-                                    },
-                                    "network": {
-                                        "encapsulation": "client",
-                                    },
-                                },
-                                "shutdown": False,
-                            }
-
-                            append_if_not_duplicate(
-                                list_of_dicts=port_channel_interfaces,
-                                primary_key="name",
-                                new_dict=port_channel_interface,
-                                context="Port-Channel Interfaces defined under point_to_point_services",
-                                context_keys=["name"],
-                            )
-
-                    else:
-                        interface = {
-                            "name": interface_name,
-                            "switchport": {"enabled": False},
-                            "peer_type": "point_to_point_service",
-                            "shutdown": False,
-                        }
-                        if point_to_point_service.lldp_disable:
-                            interface["lldp"] = {
-                                "transmit": False,
-                                "receive": False,
-                            }
-
-                        if (short_esi := endpoint.port_channel.short_esi) is not None and len(short_esi.split(":")) == 3:
-                            interface.update(
-                                {
-                                    "evpn_ethernet_segment": {
-                                        "identifier": f"{self.inputs.evpn_short_esi_prefix}{short_esi}",
-                                        "route_target": short_esi_to_route_target(short_esi),
-                                    },
-                                },
-                            )
-                            if port_channel_mode == "active":
-                                interface["lacp_id"] = short_esi.replace(":", ".")
-
-                        append_if_not_duplicate(
-                            list_of_dicts=port_channel_interfaces,
-                            primary_key="name",
-                            new_dict=interface,
-                            context="Port-Channel Interfaces defined under point_to_point_services",
-                            context_keys=["name"],
+                    if (short_esi := endpoint.port_channel.short_esi) is not None and len(short_esi.split(":")) == 3:
+                        parent_interface.evpn_ethernet_segment._update(
+                            identifier=f"{self.inputs.evpn_short_esi_prefix}{short_esi}", route_target=short_esi_to_route_target(short_esi)
                         )
+                        if port_channel_mode == "active":
+                            parent_interface.lacp_id = short_esi.replace(":", ".")
 
-            port_channel_interfaces.extend(
-                subif_parent_interface for subif_parent_interface in subif_parent_interfaces if subif_parent_interface not in port_channel_interfaces
-            )
+                    subif_parent_interfaces.add(parent_interface)
 
-        if port_channel_interfaces:
-            return port_channel_interfaces
+                    for subif in point_to_point_service.subinterfaces:
+                        subif_name = f"{interface_name}.{subif.number}"
 
-        return None
+                        po_interface = EosCliConfigGen.PortChannelInterfacesItem(
+                            name=subif_name,
+                            peer_type="point_to_point_service",
+                            shutdown=False,
+                        )
+                        po_interface.encapsulation_vlan.client._update(encapsulation="dot1q", vlan=subif.number)
+                        po_interface.encapsulation_vlan.network.encapsulation = "client"
+
+                        self.structured_config.port_channel_interfaces.append(po_interface)
+
+                else:
+                    po_interface = EosCliConfigGen.PortChannelInterfacesItem(
+                        name=interface_name,
+                        peer_type="point_to_point_service",
+                        shutdown=False,
+                    )
+                    po_interface.switchport.enabled = False
+
+                    if (short_esi := endpoint.port_channel.short_esi) is not None and len(short_esi.split(":")) == 3:
+                        po_interface.evpn_ethernet_segment._update(
+                            identifier=f"{self.inputs.evpn_short_esi_prefix}{short_esi}",
+                            route_target=short_esi_to_route_target(short_esi),
+                        )
+                        if port_channel_mode == "active":
+                            po_interface.lacp_id = short_esi.replace(":", ".")
+
+                    self.structured_config.port_channel_interfaces.append(po_interface)
