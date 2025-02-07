c25c33d99fb8cf03d4f23e4e20beefabc63f63d6
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2025-02-07 19:59:21 +0530

    Refactor(eos_designs): structured_config for overlay route_maps

--- python-avd/pyavd/_eos_designs/structured_config/overlay/route_maps.py
+++ python-avd/pyavd/_eos_designs/structured_config/overlay/route_maps.py
@@ -3,9 +3,9 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING, Protocol
 
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 from pyavd.j2filters import natural_sort
 
 if TYPE_CHECKING:
@@ -19,104 +19,57 @@ class RouteMapsMixin(Protocol):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def route_maps(self: AvdStructuredConfigOverlayProtocol) -> list | None:
-        """Return structured config for route_maps."""
+    @structured_config_contributor
+    def route_maps(self: AvdStructuredConfigOverlayProtocol) -> None:
+        """Set the structured config for route_maps."""
         if self.shared_utils.overlay_cvx:
-            return None
-
-        route_maps = []
+            return
 
         if self.shared_utils.overlay_routing_protocol == "ebgp":
             if self.inputs.evpn_prevent_readvertise_to_server:
                 remote_asns = natural_sort({rs_dict.get("bgp_as") for rs_dict in self._evpn_route_servers.values()})
                 for remote_asn in remote_asns:
-                    route_map_name = f"RM-EVPN-FILTER-AS{remote_asn}"
-                    route_maps.append(
-                        {
-                            "name": route_map_name,
-                            "sequence_numbers": [
-                                {
-                                    "sequence": 10,
-                                    "type": "deny",
-                                    "match": [f"as {remote_asn}"],
-                                },
-                                {
-                                    "sequence": 20,
-                                    "type": "permit",
-                                },
-                            ],
-                        },
-                    )
+                    route_maps_item = self.structured_config.RouteMapsItem(name=f"RM-EVPN-FILTER-AS{remote_asn}")
+                    sequence_numbers_item = route_maps_item.SequenceNumbersItem(sequence=10, type="deny")
+                    sequence_numbers_item.match.append(f"as {remote_asn}")
+                    route_maps_item.sequence_numbers.append(sequence_numbers_item)
+                    sequence_numbers_item = route_maps_item.SequenceNumbersItem(sequence=20, type="permit")
+                    route_maps_item.sequence_numbers.append(sequence_numbers_item)
+                    self.structured_config.route_maps.append(route_maps_item)
 
         elif self.shared_utils.overlay_routing_protocol == "ibgp" and self.shared_utils.overlay_vtep and self.shared_utils.evpn_role != "server":
             # Route-map IN and OUT for SOO
-            route_maps.append(
-                {
-                    "name": "RM-EVPN-SOO-IN",
-                    "sequence_numbers": [
-                        {
-                            "sequence": 10,
-                            "type": "deny",
-                            "match": ["extcommunity ECL-EVPN-SOO"],
-                        },
-                        {
-                            "sequence": 20,
-                            "type": "permit",
-                        },
-                    ],
-                },
-            )
-
-            route_maps.append(
-                {
-                    "name": "RM-EVPN-SOO-OUT",
-                    "sequence_numbers": [
-                        {
-                            "sequence": 10,
-                            "type": "permit",
-                            "set": [f"extcommunity soo {self.shared_utils.evpn_soo} additive"],
-                        },
-                    ],
-                },
-            )
+            route_maps_item = self.structured_config.RouteMapsItem(name="RM-EVPN-SOO-IN")
+            sequence_numbers_item = route_maps_item.SequenceNumbersItem(sequence=10, type="deny")
+            sequence_numbers_item.match.append("extcommunity ECL-EVPN-SOO")
+            route_maps_item.sequence_numbers.append(sequence_numbers_item)
+            sequence_numbers_item = route_maps_item.SequenceNumbersItem(sequence=20, type="permit")
+            route_maps_item.sequence_numbers.append(sequence_numbers_item)
+            self.structured_config.route_maps.append(route_maps_item)
+            route_maps_item = self.structured_config.RouteMapsItem(name="RM-EVPN-SOO-OUT")
+            sequence_numbers_item = route_maps_item.SequenceNumbersItem(sequence=10, type="permit")
+            sequence_numbers_item.set.append(f"extcommunity soo {self.shared_utils.evpn_soo} additive")
+            route_maps_item.sequence_numbers.append(sequence_numbers_item)
+            self.structured_config.route_maps.append(route_maps_item)
 
             if self.shared_utils.wan_ha:
-                route_maps.append(
-                    {
-                        "name": "RM-WAN-HA-PEER-IN",
-                        "sequence_numbers": [
-                            {
-                                "sequence": 10,
-                                "type": "permit",
-                                "description": "Set tag 50 on routes received from HA peer over EVPN",
-                                "set": ["tag 50"],
-                            },
-                        ],
-                    },
+                route_maps_item = self.structured_config.RouteMapsItem(name="RM-WAN-HA-PEER-IN")
+                sequence_numbers_item = route_maps_item.SequenceNumbersItem(
+                    sequence=10, type="permit", description="Set tag 50 on routes received from HA peer over EVPN"
                 )
-                route_maps.append(
-                    {
-                        "name": "RM-WAN-HA-PEER-OUT",
-                        "sequence_numbers": [
-                            {
-                                "sequence": 10,
-                                "type": "permit",
-                                "description": "Make EVPN routes learned from WAN less preferred on HA peer",
-                                "match": ["route-type internal"],
-                                "set": ["local-preference 50"],
-                            },
-                            {
-                                "sequence": 20,
-                                "type": "permit",
-                                "description": "Make locally injected routes less preferred on HA peer",
-                                "set": ["local-preference 75"],
-                            },
-                        ],
-                    },
+                sequence_numbers_item.set.append("tag 50")
+                route_maps_item.sequence_numbers.append(sequence_numbers_item)
+                self.structured_config.route_maps.append(route_maps_item)
+                route_maps_item = self.structured_config.RouteMapsItem(name="RM-WAN-HA-PEER-OUT")
+                sequence_numbers_item = route_maps_item.SequenceNumbersItem(
+                    sequence=10, type="permit", description="Make EVPN routes learned from WAN less preferred on HA peer"
                 )
-
-        if route_maps:
-            return route_maps
-
-        return None
+                sequence_numbers_item.match.append("route-type internal")
+                sequence_numbers_item.set.append("local-preference 50")
+                route_maps_item.sequence_numbers.append(sequence_numbers_item)
+                sequence_numbers_item = route_maps_item.SequenceNumbersItem(
+                    sequence=20, type="permit", description="Make locally injected routes less preferred on HA peer"
+                )
+                sequence_numbers_item.set.append("local-preference 75")
+                route_maps_item.sequence_numbers.append(sequence_numbers_item)
+                self.structured_config.route_maps.append(route_maps_item)
